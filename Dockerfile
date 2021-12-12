a3b00e3b803b1cb6d63638813eea8d9e104ba33c
Dan Hepworth <danhep@gmail.com>
2021-12-12 14:40:29 -0500

    Improvements to repo (#3)
    * initial commit
    
    * updated
    
    * Update README.md
    
    * Delete .env
    
    * rm unused imports

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-# syntax=docker/dockerfile:1
-FROM node:14
-WORKDIR /usr/app
-COPY package*.json ./
-RUN npm install
-COPY . .
--- deploy/hello_deploy.ts
+++ deploy/hello_deploy.ts
@@ -1,18 +1,14 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction } from 'hardhat-deploy/types';
+import { DeployFunction, DeployResult } from 'hardhat-deploy/types';
+import { deployContract } from "../utils/deploy"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {
-    deployments: { deploy },
-    getNamedAccounts,
-  } = hre;
-  const { deployer } = await getNamedAccounts();
-
-  await deploy('HelloWorlds', {
-    from: deployer,
-    log: true,
-  });
+  try {
+    const res: DeployResult = await deployContract(hre, "HelloWorlds", []);
+    console.log(`HelloWorlds on ChainID ${await hre.getChainId()} deployed to ${res.address}`);
+  } catch (error) {
+    
+  }
 };
 
 export default func;
-func.tags = ['Hello'];
